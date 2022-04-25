dc97a31cc0e4e96b6ee7b5dd7ac9a7e7be79cbc4
user <user@mbp.local>
2022-04-25 12:47:30 +0900

    build: cerate to session-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/post'
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
