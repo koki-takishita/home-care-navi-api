7daa12642723e77aa532e6ade97eb442e2d875d7
koki-takishita <tkkokitk@yahoo.co.jp>
2022-04-26 18:52:33 +0900

    jwt導入、初期設定ファイルとjwt発行ファイル作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name                  {"田中 太郎"}
-    email                 {"sample@gmail.com"}
-    password              {"1234567"}
-    password_confirmation    {"1234567"}
-    phone_number          {"11111111111"}
-    post_code             {"1111111"}
-    address               {"佐渡市秋津"}
+    name                  { '田中 太郎' }
+    email                 { 'sample@gmail.com' }
+    password              { '1234567' }
+    password_confirmation { '1234567' }
+    phone_number          { '11111111111' }
+    post_code             { '1111111' }
+    address               { '佐渡市秋津' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '新規登録' do
     before do
-      @user = FactoryBot.build(:user)
+      @user = build(:user)
     end
+
     it '全ての項目が存在すれば登録できる' do
       expect(@user).to be_valid
     end
-  it "重複したメールアドレスではアカウントが作れないこと" do
-    FactoryBot.create(:user, email: "sample@gmail.com")
-    @user = FactoryBot.build(:user, email: "sample@gmail.com")
-    @user.valid?
-    expect(@user.errors[:email]).to include("has already been taken")
-   end
-  it "重複した電話番号ではアカウントが作れないこと" do
-    FactoryBot.create(:user, phone_number: "11111111111")
-    @user = FactoryBot.build(:user, phone_number: "11111111111")
-    @user.valid?
-    expect(@user.errors[:phone_number]).to include("has already been taken")
-   end
-end
 
+    it '重複したメールアドレスではアカウントが作れないこと' do
+      create(:user, email: 'sample@gmail.com')
+      @user = build(:user, email: 'sample@gmail.com')
+      @user.valid?
+      expect(@user.errors[:email]).to include('has already been taken')
+    end
+
+    it '重複した電話番号ではアカウントが作れないこと' do
+      create(:user, phone_number: '11111111111')
+      @user = build(:user, phone_number: '11111111111')
+      @user.valid?
+      expect(@user.errors[:phone_number]).to include('has already been taken')
+    end
+  end
 end
