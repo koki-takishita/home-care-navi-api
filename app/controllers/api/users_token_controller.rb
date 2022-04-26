f524bd44e1bd664f63bd20486216f449f73b9df0
koki-takishita <tkkokitk@yahoo.co.jp>
2022-04-26 23:13:06 +0900

    users_token_controller 作成

---
+++ app/controllers/api/users_token_controller.rb
@@ -0,0 +1,51 @@
+class Api::UsersTokenController < ApplicationController
+  rescue_from UserAuth.not_found_exception_class, with: :not_found
+
+  before_action :delete_cookie
+  before_action :authenticate, only: [:create]
+
+  def create
+    cookies[token_access_key] = cookie_token
+    render json: {
+      exp: auth.payload[:exp],
+      user: entity.my_json
+    }
+  end
+
+  def destroy
+    head(:ok)
+  end
+
+  private
+
+    def entity
+      @_entiry ||= User.find_activated(auth_params[:email])
+    end
+
+    def auth_params
+      params.require(:auth).permit(:email, :passowrd)
+    end
+
+    def auth
+      @_auth ||= UserAuth::AuthToken.new(payload: { sub: entity.id })
+    end
+
+    def cookie_token
+      {
+        value: auth.token,
+        expires: Time.zone.at(auth.payload[:exp]),
+        secure: Rails.env.production?,
+        http_only: true
+      }
+    end
+
+    def authenticate
+      unless entity.present? && entity.authenticate(auth_params[:password])
+        raise UserAuth.not_found_exception_class
+      end
+    end
+
+    def not_found
+      head(:not_found)
+    end
+end
