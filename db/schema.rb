fdb15dd0aa257799938b97128c8c82e856ad8e03
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-09 15:21:20 +0900

    build: create office model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_013022) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_09_061906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offices", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.integer "holiday"
+    t.string "business_day_detail"
+    t.string "address"
+    t.integer "post_code"
+    t.integer "phone_number"
+    t.integer "fax_number"
+    t.integer "user_id"
+    t.integer "specialist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
