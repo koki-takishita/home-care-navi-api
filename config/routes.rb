e62726ce815a761f01e4e830e13b6edcad30f68f
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-09 11:08:34 +0900

    既存のルーティング削除 必要ないため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks]
-  namespace :api do
-    resources :users, only: [:create]
-  end
 end
