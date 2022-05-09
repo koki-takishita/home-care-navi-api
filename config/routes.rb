82bddda8c8821c9c626bcea4f5dcb77b843d6a58
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-09 10:44:24 +0900

    devise_token_auth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks]
   namespace :api do
     resources :users, only: [:create]
   end
