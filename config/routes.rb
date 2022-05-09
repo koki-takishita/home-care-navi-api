5f5a838434a3f46765092893e24636d2bf0709f7
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-09 12:38:52 +0900

    build: create offices controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :offices, only: [:create, :index]
+  end
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks], controllers: {
     registrations: 'api/overrides/registrations'
   }
