7ec9ef79b5fe0f5c1c35a8316c6175060109b36c
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-09 14:33:21 +0900

    ユーザーモデルの属性日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-            # Include default devise modules.
+  # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable
   include DeviseTokenAuth::Concerns::User
+  validates :name, :phone_number, :post_code, :address, :email, presence: true
+  validates :email, uniqueness: true
 end
