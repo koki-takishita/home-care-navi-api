a90bcf157f2f9cff17a674eea9c4e29c41ffe08b
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-09 16:12:06 +0900

    nuxt側からアクセスできるようにcors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8000'
-
     resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :options, :delete, :put]
   end
 end
