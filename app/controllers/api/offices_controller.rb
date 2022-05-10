b112491dc35c8072b45bd67d7b4d3279cb55bf01
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-10 17:29:57 +0900

    事業所データ取得

--- app/controllers/api/offices_controller.rb
+++ app/controllers/api/offices_controller.rb
@@ -1,7 +1,20 @@
 class Api::OfficesController < ApplicationController
+  before_action :set_office, only: [:show]
+
   def index
+    offices = Office.all
+    render json:{ data: offices }
+  end
+
+  def show
+    render json: { data: @office }
   end
 
   def create
   end
+
+  private
+   def set_office
+      @office = Office.find(params[:id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :offices, only: [:create, :index]
+    resources :offices, only: [:create, :index, :show]
   end
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks], controllers: {
     registrations: 'api/overrides/registrations'
