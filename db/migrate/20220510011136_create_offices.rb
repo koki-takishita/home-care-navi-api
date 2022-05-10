bd77df80c9dcba2517562bad3825e58aab37f7b5
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-10 10:16:03 +0900

    build: create office model

---
+++ db/migrate/20220510011136_create_offices.rb
@@ -0,0 +1,17 @@
+class CreateOffices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :offices do |t|
+      t.string :name
+      t.string :title
+      t.integer :holiday
+      t.string :business_day_detail
+      t.string :address
+      t.integer :post_code
+      t.integer :phone_number
+      t.integer :fax_number
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
