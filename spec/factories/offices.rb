1702a664874dd22fcaf35c4503332ffe80fd2951
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-10 10:58:19 +0900

    build: create office model

---
+++ spec/factories/offices.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :office do
+    name { "MyString" }
+    title { "MyString" }
+    holiday { 1 }
+    business_day_detail { "MyString" }
+    address { "MyString" }
+    post_code { 1 }
+    phone_number { 1 }
+    fax_number { 1 }
+    user_id { 1 }
+  end
+end
