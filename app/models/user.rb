fdd7e5f7787111121931439b8337bf75526d53b1
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-16 11:45:51 +0900

    postmanからログイン・ログアウト処理ができる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,23 @@
 class User < ApplicationRecord
 
-  enum user_type: { customer: 0, specialist: 1 }
-            # Include default devise modules.
-
-  # Include default devise modules.
-
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
+          :recoverable, :rememberable, :validatable, :confirmable
+
   include DeviseTokenAuth::Concerns::User
+
+  enum user_type: { customer: 0, specialist: 1 }
+
+  validates :name, :phone_number, :post_code, :address, :email, presence: true
+  validates :phone_number, uniqueness: true
 
   def set_user_type(type)
     set_specialist if type == :specialist
   end
 
+  private
+
   def set_specialist
     self.specialist!
   end
-
-  validates :name, :phone_number, :post_code, :address, :email, presence: true
-  validates :phone_number, uniqueness: true
 
 end
