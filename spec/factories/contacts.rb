0acac6df1b8f76aa52ddb5dc39079dffccf82216
tmhr0 <falsity_0330@yahoo.co.jp>
2022-05-17 15:09:29 +0900

    テストデータ系削除

--- spec/factories/contacts.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-    factory :contact do
-      name                  {"田中 太郎"}
-      email                 {"sample@gmail.com"}
-      types                  {"ユーザー"}
-      content               {"HELLO!!"}
-    end
-  end
