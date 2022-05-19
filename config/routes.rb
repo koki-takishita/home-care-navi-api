ce112f7ef9994b9db82196a23d85ad5a3fd79750
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-19 11:54:55 +0900

    build: スタッフ登録(画像抜き)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :offices, only: [:index, :show]
+    namespace :specialists do
+      resources :staffs, only: [:create]
+    end
   end
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks], controllers: {
     registrations: 'api/overrides/registrations'
