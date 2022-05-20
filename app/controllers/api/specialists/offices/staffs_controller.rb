82aa86856bfa551c8f0dcc4c679889325bb02e5d
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-20 10:18:56 +0900

    fix: スタッフエンドポイント修正

--- app/controllers/api/specialists/offices/staffs_controller.rb
+++ app/controllers/api/specialists/offices/staffs_controller.rb
@@ -1,4 +1,9 @@
 class Api::Specialists::StaffsController < ApplicationController
+  def index
+    staffs = Staff.all
+    render json: staffs
+  end
+
   def create
     staff = Staff.new(staff_params)
     if staff.valid?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     resources :offices, only: [:index, :show]
     namespace :specialists do
-      resources :staffs, only: [:create]
+      resources :offices do
+        resources :staffs, only: [:index, :create]
+      end
     end
   end
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks], controllers: {
