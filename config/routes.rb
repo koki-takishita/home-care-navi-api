99450072fca36d4dc8d4f7cf2a4ba334385e2fff
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-05-23 16:09:56 +0900

    ”fix:specialists-routes”

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :offices, only: [:index, :show]
-  end
+
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks], controllers: {
     registrations: 'api/overrides/registrations'
   }
-  devise_scope :user do
+
+  mount_devise_token_auth_for "Specialist", at: 'api/specialist'
+  devise_scope :specialist do
     post 'api/specialists/users', to: 'api/overrides/specialist_registrations#create'
+  end
+  
+  namespace :api do
+    resources :offices, only: [:index, :show]
+    resource :specialists do
+      resources :offices ,controller: 'specialists/offices' do
+        resources :staffs, controller: 'specialists/staffs', only: [:index, :create]
+      end
+    end
   end
 end
