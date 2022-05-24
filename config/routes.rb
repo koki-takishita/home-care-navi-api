2c3101ac388dd670df78cab5235e22f69217c311
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-05-24 11:54:27 +0900

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :offices, only: [:index, :show]
-    namespace :specialists do
-      resources :staffs, only: [:create]
-    end
   end
   mount_devise_token_auth_for 'User', at: 'api/users', skip: [:omniauth_callbacks, :sessions], controllers: {
     registrations: 'api/overrides/registrations'
   }
   devise_scope :user do
+  post   'api/login',           to: 'devise_token_auth/sessions#create'
+  delete 'api/logout',           to: 'devise_token_auth/sessions#destroy'
+  end
+
+  mount_devise_token_auth_for "Specialist", at: 'api/specialist'
+  devise_scope :specialist do
     post 'api/specialists/users', to: 'api/overrides/specialist_registrations#create'
-    post   'api/login',           to: 'devise_token_auth/sessions#create'
-    delete 'api/logout',           to: 'devise_token_auth/sessions#destroy'
+  end
+  
+  namespace :api do
+    resource :specialists do
+      resources :offices ,controller: 'specialists/offices' do
+        resources :staffs, controller: 'specialists/staffs', only: [:index, :create]
+      end
+    end
   end
 end
