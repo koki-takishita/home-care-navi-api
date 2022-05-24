517db3f0ff8ef716f13040eeed07812b6fcae0ce
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-24 21:20:52 +0900

    build: スタッフ登録-画像あり

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,10 @@
 class Staff < ApplicationRecord
+  include Rails.application.routes.url_helpers
   has_many :office
   has_one_attached :image
+
+  def image_url
+      helpers = Rails.application.routes.url_helpers
+      helpers.url_for(image)
+  end
 end
