b7720d1c1456318e10a544c402081fce909e113c
tmhr0 <falsity_0330@yahoo.co.jp>
2022-05-31 15:27:20 +0900

    入力データの登録

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,17 +1,25 @@
 module Api
   class ContactsController < ApplicationController
     def create
-    render json: {
-#     contacts = Contacts.new(contacts_params)
-      contacts = params[:contacts].permit(:name, :email, :types, :content)
-      Contacts.create(contacts)
-    }
+      @contacts = Contact.new(contacts_params)
+
+      if @contacts.valid?
+        @contacts.save!
+      else
+        render json: { status: @contacts.errors.full_messages }
+      end
+    end
+#      contacts = params[:contacts].permit(:name, :email, :types, :content)
+#      Contacts.create(contacts)
 
 #  render json: {
 #    test: "true"
 #    }
+private
+  def contacts_params
+    params.permit(:name, :email, :types, :content)
+  end
 
-    end
   end
 end
 
