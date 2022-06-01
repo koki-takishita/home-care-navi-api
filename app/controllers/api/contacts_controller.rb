28d55c4adb579cd305ba535777ea0699d1debe5a
tmhr0 <falsity_0330@yahoo.co.jp>
2022-06-01 15:44:32 +0900

    fix:ステータス表示とチェンジテーブル名変更

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -5,6 +5,8 @@ module Api
 
       if @contacts.valid?
         @contacts.save!
+        render json: @contacts, status: 200
+        ContactMailer.send_message(@contacts.name).deliver_now
       else
         render json: { status: @contacts.errors.full_messages }
       end
--- db/migrate/20220531230946_change.rb
+++
@@ -1,9 +0,0 @@
-class Change < ActiveRecord::Migration[7.0]
-  def up
-    change_column :contacts, :types, :string
-  end
-
-  def down
-    change_column :contacts, :types, :integer
-  end
-end
