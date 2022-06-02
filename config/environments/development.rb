4ac3dbf15775b7894a55f148ced36cab3c5beca4
tmhr0 <falsity_0330@yahoo.co.jp>
2022-06-02 15:42:15 +0900

    プルリク用に修正3回目

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   # email認証リファレンス https://devise-token-auth.gitbook.io/devise-token-auth/config/email_auth
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => 'smtp', :port => 1025 }
