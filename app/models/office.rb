eaa6c22bb88027341eb2f058d550c8719f33d6cb
koki-takishita <tkkokitk@yahoo.co.jp>
2022-06-08 20:12:00 +0900

    create relation

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,7 +1,7 @@
 class Office < ApplicationRecord
   include FlagShihTzu
-  belongs_to :user
-  has_many :staff
+  belongs_to :specialist
+  has_many :staff, dependent: :destroy
   has_many :thanks, dependent: :destroy
   has_one  :office_detail, dependent: :destroy
 
