e17abead24b0d5662369acb636e463b39a03c16e
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-06-22 14:52:48 +0900

    fix: indexメソッド修正

--- app/controllers/api/specialists/appointments_controller.rb
+++ app/controllers/api/specialists/appointments_controller.rb
@@ -1,13 +1,7 @@
 class Api::Specialists::AppointmentsController < ApplicationController
   include ActionController::MimeResponds
   def index
-    # オフィス.予約.user
-    # ユーザーに紐づく予約でなく、予約に紐づくユーザー
     @appointments = current_specialist.office.appointments
-    @user_id = @appointments.pluck(:user_id)
-    @user = User.find(@user_id)
-    render json: @user.to_json(:include => [:appointments])
-    # render json: @user.to_json(:include => [:appointments])
-    # render json: { appointments: @appointments, include: [:user] }
+    render json: { appointments: @appointments }
   end
 end
