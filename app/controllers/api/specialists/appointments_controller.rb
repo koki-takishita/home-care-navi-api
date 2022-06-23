7177d8c48d6784f6becff0952d2e87e356e41114
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-06-23 10:11:53 +0900

    build: 予約seed作成 routeとindexメソッド修正

--- app/controllers/api/specialists/appointments_controller.rb
+++ app/controllers/api/specialists/appointments_controller.rb
@@ -1,7 +1,8 @@
 class Api::Specialists::AppointmentsController < ApplicationController
   include ActionController::MimeResponds
   def index
-    @appointments = current_specialist.office.appointments
-    render json: { appointments: @appointments }
+    data_length = current_specialist.office.appointments.length
+    @appointments = current_specialist.office.appointments.limit(10).offset(params[:page].to_i * 10)
+    render json: { appointments: @appointments, data_length: data_length }
   end
 end
