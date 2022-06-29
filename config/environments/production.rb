1ea2f5989912e473ee5e91cf5c30505d3914d372
koki-takishita <48609992+koki-takishita@users.noreply.github.com>
2022-06-29 17:31:14 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,17 +67,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'home-care-navi-api-v2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'hogehoge,
-    password:             'hogehoge',
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
