46283cd8154fd0442e78e8c07f0715c56f4921fa
tmhr0 <falsity_0330@yahoo.co.jp>
2022-06-30 16:33:50 +0900

    必須系のバリデーション設定

--- app/models/office_detail.rb
+++ app/models/office_detail.rb
@@ -1,4 +1,6 @@
 class OfficeDetail < ApplicationRecord
   belongs_to :office, foreign_key: 'office_id',
   dependent: :destroy
+  validates :office_id, uniqueness: true
+  validates :detail, :service_type, presence: true
 end
