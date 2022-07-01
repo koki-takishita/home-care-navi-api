805ae431f8c47b8f58fc83fa694e9744cfe32464
tmhr0 <falsity_0330@yahoo.co.jp>
2022-07-01 10:27:19 +0900

    外部キー設定削除

--- app/models/office_detail.rb
+++ app/models/office_detail.rb
@@ -1,5 +1,4 @@
 class OfficeDetail < ApplicationRecord
-  belongs_to :office, foreign_key: 'office_id',
-  dependent: :destroy
+  belongs_to :office,dependent: :destroy
   validates :detail, :service_type, presence: true
 end
