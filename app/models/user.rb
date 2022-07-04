2b0a806445233cb52c9ac5cee38b7259e979f9e4
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-04 15:34:35 +0900

    fix: 関連付け不要なコード削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_one :office, foreign_key: 'user_id', dependent: :destroy
   has_many :thanks, dependent: :destroy
-  has_one :office, dependent: :destroy
 
   enum user_type: { customer: 0, specialist: 1 }
 
