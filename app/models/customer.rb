e00549d7a0d9dfbaff819bfe99a1af8d25276004
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-05 16:48:09 +0900

    フロントのテストがパスする

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < User
   has_many :appointments, dependent: :destroy
-  has_many :thanks, dependent: :destroy
+  has_many :thanks, foreign_key: 'user_id', dependent: :destroy
   enum user_type: { customer: 0 }
   default_scope { where(user_type: :customer) }
 end
