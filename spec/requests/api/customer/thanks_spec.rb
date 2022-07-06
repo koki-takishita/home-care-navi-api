7523320020c14c23c6191e11b4c980980fcceb76
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-06 16:21:41 +0900

    お礼のテスト作成完了

--- spec/requests/api/customer/thanks_spec.rb
+++ spec/requests/api/customer/thanks_spec.rb
@@ -11,23 +11,67 @@ RSpec.describe "Api::Customer::Thanks", type: :request do
     @office = create(:office, :with_staffs)
     @staff  = Staff.find_by(office_id: @office.id)
     @thank  = build(:thank, user: @customer, office: @office, staff: @staff)
+
+    specialist = build(:specialist)
+    specialist.skip_confirmation!
+    specialist.save
+    @specialist = Specialist.find_by_id(specialist.id)
+    @thank2 = build(:thank, user: @specialist, office: @office, staff: @staff)
   end
 
-  it "if you are logged in can create thank msg" do
-		login(@customer)
-		auth_params = get_auth_params_from_login_response_headers(response)
+  context 'ログイン済み' do
+    context 'カスタマー' do
+      it "お礼を作成できる" do
+	    	login(@customer)
+	    	auth_params = get_auth_params_from_login_response_headers(response)
 
-    expect {
-      post api_office_thanks_path(@thank.office_id),
-      params: {
-        thank: {
-            comments: @thank.comments,
-            office_id: @thank.office_id,
-            staff_id: @thank.staff_id
-          }
-      },
-      headers: auth_params
-    }.to change(@customer.thanks, :count).by(1)
+        expect {
+          post api_office_thanks_path(@thank.office_id),
+          params: {
+            thank: {
+                comments: @thank.comments,
+                office_id: @thank.office_id,
+                staff_id: @thank.staff_id
+              }
+          },
+          headers: auth_params
+        }.to change(@customer.thanks, :count).by(1)
+      end
+    end
+
+    context 'ケアマネ' do
+      it "お礼を作成できない" do
+	    	login(@specialist)
+	    	auth_params = get_auth_params_from_login_response_headers(response)
+
+        expect {
+          post api_office_thanks_path(@thank.office_id),
+          params: {
+            thank: {
+                comments: @thank.comments,
+                office_id: @thank.office_id,
+                staff_id: @thank.staff_id
+              }
+          },
+          headers: auth_params
+        }.to change(@specialist.thanks, :count).by(0)
+      end
+    end
+  end
+
+  context 'ログインしていない' do
+    it 'お礼を作成できない' do
+      expect {
+        post api_office_thanks_path(@thank.office_id),
+        params: {
+          thank: {
+              comments: @thank.comments,
+              office_id: @thank.office_id,
+              staff_id: @thank.staff_id
+            }
+        }
+      }.to change(@specialist.thanks, :count).by(0)
+    end
   end
 
 	def login(user)
@@ -53,5 +97,4 @@ RSpec.describe "Api::Customer::Thanks", type: :request do
     }
     auth_params
   end
-
 end
