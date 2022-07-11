913572097a1df8700569374392e6c89cf7714256
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-11 23:45:35 +0900

    fix バリテーション設定

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -3,7 +3,7 @@ class Thank < ApplicationRecord
   belongs_to :office
   belongs_to :staff
   validates :comments, presence: true, length: { maximum: 120 }
-  validates :comments, uniqueness: {
+  validates :office_id, uniqueness: {
     scope: [ :user_id, :staff_id ],
       message: "に2回以上お礼は作成できません。"
     }
