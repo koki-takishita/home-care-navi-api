67004742a8184b57b20d735bb22062ad02759a74
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-07-13 12:09:47 +0900

    build: プッシュ時にテストが走る

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :appointment do
-    meet_date     { Time.parse("2023/01/01") }
+    from = Time.parse("2023/01/01")
+    to = Time.parse("2023/12/31")
+    meet_date     { rand(from..to) }
     name          { Faker::Name.name }
     age           { rand(60..120) }
     phone_number  { Faker::PhoneNumber.cell_phone }
