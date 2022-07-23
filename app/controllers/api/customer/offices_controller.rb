5bbd297ed08894d3992bbeb70e083c9b82e5746c
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-23 18:01:32 +0900

    fix: 並び替え修正

--- app/controllers/api/customer/offices_controller.rb
+++ app/controllers/api/customer/offices_controller.rb
@@ -24,10 +24,9 @@ class Api::Customer::OfficesController < ApplicationController
 
   def set_office
     @office = Office.find(params[:id])
-    @staffs = @office.staffs.select('staffs.*', 'count(staffs.id) AS thanks')
+    @staffs = @office.staffs.select('staffs.*', 'count(thanks.id) AS thanks')
                      .left_joins(:thanks)
                      .group("staffs.id").order('thanks DESC')
-    @thanks = @office.thanks
   end
 
   def add_image_h(records)
