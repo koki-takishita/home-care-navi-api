7f00d8914bdce5f13c896a2882c7ab58f4e26985
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-07-25 17:11:39 +0900

    fix: 予約一覧 jsonの組み直し パフォーマンス向上

--- app/controllers/api/customer/appointments_controller.rb
+++ app/controllers/api/customer/appointments_controller.rb
@@ -1,11 +1,16 @@
-class Api::Customer::AppointmentsController < ApplicationController
+class Api::Customer::AppointmentsController < Api::Customer::OfficesController
   before_action :authenticate_user!
   before_action :set_office, only: [:create]
 
 	def index
     appointments = current_user.appointments.order(created_at: :desc)
     offices = get_office_from_appointments(appointments)
-    render json: offices, :include => {:appointments => {:only => [:meet_date, :meet_time, :called_status, :user_id, :created_at, :updated_at]}, :staffs => {:only => [:id]}}, methods: [:image_url]
+		result = if offices.size <= 0
+                []
+             else
+							build_json(offices)
+             end
+						 render json: result
   end
 
   def create
@@ -35,5 +40,28 @@ class Api::Customer::AppointmentsController < ApplicationController
 			}
 			offices = Office.find(offices_id)
 			offices
+		end
+
+		def build_json(offices)
+			result = offices.each_with_index.map{|office|
+				superClass  = Api::Customer::AppointmentsController.new
+				image       = superClass.build_json_image(office)
+				staff_count = superClass.build_json_from_staff_table_count_json(office)
+				appointment = build_json_from_appointments_table_json(office)
+				office      = office.attributes
+
+				office.merge(image, staff_count, appointment)
+			 }
+			 result
+		end
+
+		def build_json_from_appointments_table_json(office)
+			if office.appointments.exists?
+        appointments = office.appointments.where(user_id: current_customer.id)
+				latest_office_appointment = appointments.order(created_at: :desc).first
+        { appointment: latest_office_appointment }
+      else
+        { appointment: nil}
+      end
 		end
 end
