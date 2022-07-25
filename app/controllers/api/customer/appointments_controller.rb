06088bc3ab86a530d8629fec9cc0e0300ac7200f
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-07-25 16:08:09 +0900

    build: 予約専用seed作成 メソッド修正

--- app/controllers/api/customer/appointments_controller.rb
+++ app/controllers/api/customer/appointments_controller.rb
@@ -2,6 +2,12 @@ class Api::Customer::AppointmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_office, only: [:create]
 
+	def index
+    appointments = current_user.appointments.order(created_at: :desc)
+    offices = get_office_from_appointments(appointments)
+    render json: offices, :include => {:appointments => {:only => [:meet_date, :meet_time, :called_status, :user_id, :created_at, :updated_at]}, :staffs => {:only => [:id]}}, methods: [:image_url]
+  end
+
   def create
     params[:user_id] = current_user.id
     appointment = Appointment.new(appointment_params)
@@ -21,4 +27,13 @@ class Api::Customer::AppointmentsController < ApplicationController
     def set_office
       @office = Office.find(params[:office_id])
     end
+
+		def get_office_from_appointments(appointments)
+			offices_id = []
+			appointments.each_with_index.map{|appointment, index|
+				offices_id.push(appointment.office_id)
+			}
+			offices = Office.find(offices_id)
+			offices
+		end
 end
---
+++ db/seeds/dummy_appointments.seeds.rb
@@ -0,0 +1,60 @@
+# customerが10人いる前提
+def create_appointments(count: Customer.count, office: nil)
+  customers = User.customer
+  unless(customers.count > 0)
+    e = <<-TEXT
+    customerの数が足りません
+    最後まで実行したい場合1か2の指示に従ってください。
+    1. rails db:seed:dummy_customer
+    2. customerを10人以上作成してください
+    TEXT
+
+    raise e
+  end
+
+	from = Time.parse("2023/01/01")
+	to = Time.parse("2023/12/31")
+	timezone_from = Time.zone.parse('2010-01-01 00:00:00')
+	timezone_to   = Time.zone.parse('2021-12-31 00:00:00')
+	random_timezone = rand(timezone_from..timezone_to)
+
+  count.times{|n|
+    customers[n].appointments.create!(
+      meet_date:     rand(from..to),
+      meet_time:     "18:00〜20:00",
+      name:          Faker::Name.name,
+      age:           rand(60..120),
+      phone_number:  Faker::PhoneNumber.cell_phone,
+      comment:       "お困りごと",
+      called_status: rand(0..2),
+      office_id:     office.id,
+      created_at: random_timezone,
+			updated_at: random_timezone
+    )
+  }
+end
+
+def set_progressbar(total: nil)
+  @progressbar = ProgressBar.create(:format => '%a |%b>>%i| %p%% %t',
+                                   :starting_at => 0,
+                                   :total => total,
+                                   :length => 80)
+end
+
+def create_customer_one_office_appointments
+  Appointment.destroy_all
+  office = Specialist.first.office
+  create_appointments(office: office)
+end
+
+def create_customer_multiple_office_appointments
+  offices = Office.eager_load(:staffs).with_attached_images.limit(10)
+  set_progressbar(total: offices.count)
+  offices.each{|office|
+    create_appointments(office: office)
+    @progressbar.increment
+  }
+end
+
+create_customer_one_office_appointments
+create_customer_multiple_office_appointments
