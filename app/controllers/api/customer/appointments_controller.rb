58559490157f16916a4c080aef66c9571c5024fd
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-07-26 11:44:31 +0900

    fix: 文法ミス修正

--- app/controllers/api/customer/appointments_controller.rb
+++ app/controllers/api/customer/appointments_controller.rb
@@ -58,7 +58,8 @@ class Api::Customer::AppointmentsController < Api::Customer::OfficesController
     def build_json_from_appointments_table_json(office)
       if office.appointments.exists?
       appointments = office.appointments.where(user_id: current_customer.id)
-       latest_office_appointment = appointments.order(created_at: :desc).build_json_from_appointments_table_js    { appointment: latest_office_appointment }
+      latest_office_appointment = appointments.order(created_at: :desc).first
+			{ appointment: latest_office_appointment }
       else
        { appointment: nil}
       end
