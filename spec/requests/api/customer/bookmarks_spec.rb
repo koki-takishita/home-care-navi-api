4bc0ce8520c16c4dd948e563c009b41551ff0f13
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-07-28 14:03:06 +0900

    fix: インデント修正

--- spec/requests/api/customer/bookmarks_spec.rb
+++ spec/requests/api/customer/bookmarks_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe "Api::Customer::Bookmarks", type: :request do
 
   context 'ログイン済み' do
     context 'カスタマー' do
-			let(:auth_params) { login(@customer) }
-			let(:bookmark) { create(:bookmark, user: @customer) }
+      let(:auth_params) { login(@customer) }
+      let(:bookmark) { create(:bookmark, user: @customer) }
       it "ブックマークを登録できる" do
           post api_office_bookmarks_path(@bookmark.office_id),
           params: {
@@ -31,10 +31,10 @@ RSpec.describe "Api::Customer::Bookmarks", type: :request do
 
           getBookmark = Customer.first.bookmarks
           expect(getBookmark.count).to eq(1)
-					expect(response).to have_http_status(200)
+          expect(response).to have_http_status(200)
       end
-			it "ブックマークを解除できる" do
-				bookmark
+      it "ブックマークを解除できる" do
+          bookmark
           delete api_office_bookmark_path(bookmark.office_id, bookmark.id),
           params: {
             office_id: bookmark.office_id,
@@ -43,12 +43,12 @@ RSpec.describe "Api::Customer::Bookmarks", type: :request do
           headers: auth_params
 
           expect(@customer.bookmarks.count).to eq(0)
-					expect(response).to have_http_status(200)
+          expect(response).to have_http_status(200)
       end
     end
 
     context 'ケアマネ' do
-			let(:auth_params) { login(@specialist) }
+      let(:auth_params) { login(@specialist) }
       it "ブックマークを登録できない" do
         post api_office_bookmarks_path(@bookmark.office_id),
           params: {
@@ -59,13 +59,13 @@ RSpec.describe "Api::Customer::Bookmarks", type: :request do
 
           getBookmark = Specialist.first.bookmarks
           expect(getBookmark.count).to eq(0)
-					expect(response).to have_http_status(401)
+         expect(response).to have_http_status(401)
       end
     end
   end
 
   context 'ログインしていない' do
-		let(:bookmark) { create(:bookmark, user: @customer) }
+    let(:bookmark) { create(:bookmark, user: @customer) }
     it 'ブックマークを登録できない' do
       post api_office_bookmarks_path(@bookmark.office_id),
         params: {
@@ -75,7 +75,7 @@ RSpec.describe "Api::Customer::Bookmarks", type: :request do
 
         getBookmark = Customer.first.bookmarks
         expect(getBookmark.count).to eq(0)
-				expect(response).to have_http_status(401)
+        expect(response).to have_http_status(401)
     end
   end
 end
