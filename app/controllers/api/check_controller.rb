0ad1735127f6a141ac40b3a9434e6ebfabb6ae08
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-08 08:24:20 +0900

    電話番号チェックプログラム完成

---
+++ app/controllers/api/check_controller.rb
@@ -0,0 +1,34 @@
+module Api
+  class CheckController < ApplicationController
+
+    def check_phone_number
+      phone_number = check_params[:phone_number]
+      res = !phone_number_exist?(phone_number)
+      res ? render_create_success : render_error
+    end
+
+    private
+
+    def check_params
+      params.require(:check).permit(:phone_number)
+    end
+
+    # User or Officeにphone_numerが存在するならtrue
+    def phone_number_exist?(phone_number)
+      Office.phone_number_exist?(phone_number).exists? || User.phone_number_exist?(phone_number).exists?
+    end
+
+    def render_create_success
+      render json: {
+        message: 'officesとusersには電話番号は存在しないです。'
+      }, status: :ok
+    end
+
+    def render_error
+      render json: {
+        message: '登録済みの電話番号です。',
+      }, status: 403
+    end
+
+  end
+end
---
+++ spec/requests/api/check_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Checks", type: :request do
+  describe "GET /check-phone-number" do
+
+    before do
+      create(:customer,   phone_number: '080-0000-0000')
+      create(:specialist, phone_number: '080-0000-0001')
+      create(:office,     phone_number: '080-0000-0002')
+    end
+
+    context 'usersに登録済みの電話番号' do
+      it "403が返ってくる" do
+        get '/api/check-phone-number', :params => { :check => { phone_number: '080-0000-0000' } }
+        expect(response).to have_http_status(403)
+      end
+
+      it "403が返ってくる" do
+        get '/api/check-phone-number', :params => { :check => { phone_number: '080-0000-0001' } }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    context 'officesに登録済みの電話番号' do
+      it "403が返ってくる" do
+        get '/api/check-phone-number', :params => { :check => { phone_number: '080-0000-0002' } }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    context '未登録の電話番号' do
+      it "200が返ってくる" do
+        get '/api/check-phone-number', :params => { :check => { phone_number: '080-0000-0003' } }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
