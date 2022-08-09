d946d337b4ea9c86b0e9398511ffa84758b7feef
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-09 10:58:04 +0900

    build: テスト項目追加

--- spec/requests/api/customer/histories_spec.rb
+++ spec/requests/api/customer/histories_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::Customer::Bookmarks', type: :request do
+RSpec.describe 'Api::Customer::Histories', type: :request do
   before do
     customer = build(:customer)
     customer.skip_confirmation!
@@ -22,10 +22,10 @@ RSpec.describe 'Api::Customer::Bookmarks', type: :request do
       let(:history) { create(:history, user: @customer) }
 
       it '閲覧データを登録できる' do
-        post api_office_histories_path(@history.office.id),
+        post api_office_histories_path(@history.office_id),
              params: {
                user_id: @customer.id,
-               office_id: @history.office.id
+               office_id: @history.office_id
              },
              headers: auth_params
 
@@ -49,10 +49,10 @@ RSpec.describe 'Api::Customer::Bookmarks', type: :request do
     context 'ケアマネ' do
       it '閲覧データを登録できない' do
         auth_params = login(@specialist)
-        post api_office_histories_path(@history.office.id),
+        post api_office_histories_path(@history2.office_id),
              params: {
                user_id: @specialist.id,
-               office_id: @history.office.id
+               office_id: @history2.office_id
              },
              headers: auth_params
 
@@ -60,6 +60,22 @@ RSpec.describe 'Api::Customer::Bookmarks', type: :request do
         expect(history.count).to eq(0)
         expect(response).to have_http_status(:unauthorized)
       end
+    end
+  end
+
+  context 'ログインしていない' do
+    let(:history) { create(:history, user: @customer) }
+
+    it '閲覧データを登録できない' do
+      post api_office_histories_path(@history.office_id),
+           params: {
+             user_id: @customer.id,
+             office_id: @history.office_id
+           }
+
+      history = @customer.histories
+      expect(history.count).to eq(0)
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
