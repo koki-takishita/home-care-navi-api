72b5c65d1f9463db8c0cb1f928d2c9d72210b018
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-16 11:19:33 +0900

    build: スタッフモデルテスト作成 バリデーション設定

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -4,10 +4,17 @@ class Staff < ApplicationRecord
   has_one_attached :image
   has_many :thanks, dependent: :destroy
 
+  validates :name,  presence: true, length: { maximum: 30 }
+  VALID_KANA_REGEX = /\A[ぁ-んー－　 ]+\z/.freeze
+  validates :kana,  presence: true,
+                    format: { with: VALID_KANA_REGEX },
+                    length: { maximum: 30 }
+  validates :introduction,  presence: true, length: { maximum: 120 }
+
   def image_url
     helpers = Rails.application.routes.url_helpers
     if image.blank?
-      return
+      nil
     else
       helpers.url_for(image)
     end
