5418866fe28ebd091fcf2ea61150608e2131ebfb
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-18 16:53:40 +0900

    build: スタッフ一覧と詳細のリクエストスペック作成

--- app/controllers/api/specialists/staffs_controller.rb
+++ app/controllers/api/specialists/staffs_controller.rb
@@ -5,12 +5,8 @@ class Api::Specialists::StaffsController < ApplicationController
   def index
     @order_staffs = current_specialist.office.staffs.order(updated_at: :desc)
     @data_length = @order_staffs.count
-    @staffs = if params[:page].blank?
-                @order_staffs
-              else
-                @staffs = @order_staffs.limit(10).offset(params[:page].to_i * 10)
-                render json: { staffs: @staffs, data_length: @data_length }, methods: [:image_url]
-              end
+    @staffs = @order_staffs.limit(10).offset(params[:page].to_i * 10)
+    render json: { staffs: @staffs, data_length: @data_length }, methods: [:image_url]
   end
 
   def show
