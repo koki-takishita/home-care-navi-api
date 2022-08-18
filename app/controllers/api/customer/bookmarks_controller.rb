7a3adcd078a6dc6d2d15aaf81089215e2d23662e
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-18 14:05:57 +0900

    fix: ブックマークコントローラのリファクタリング

--- app/controllers/api/customer/bookmarks_controller.rb
+++ app/controllers/api/customer/bookmarks_controller.rb
@@ -1,93 +1,72 @@
-class Api::Customer::BookmarksController < Api::Customer::OfficesController
-  before_action :authenticate_customer!, only: [:create, :destroy]
+class Api::Customer::BookmarksController < ApplicationController
+  before_action :authenticate_customer!, only: %i[create destroy]
   before_action :set_bookmark, only: [:destroy]
 
   def index
-    if customer_signed_in?
-      bookmarks = current_customer.bookmarks.order(created_at: :desc).limit(10).offset(params[:page].to_i * 10)
-      offices = get_office_from_bookmarks(bookmarks)
-      result = if(offices.size <= 0)
-        []
-      else
-        build_json(offices)
-      end
-      render json: result
-    else
-      render json: []
-    end
+    bookmarks = current_customer.bookmarks.order(created_at: :desc).limit(10).offset(params[:page].to_i * 10)
+    offices = get_office_from_bookmarks(bookmarks)
+    result = if offices.count <= 0
+               []
+             else
+               build_json(offices)
+             end
+    render json: result
   end
 
   def create
     customer = current_customer
     bookmark = customer.bookmarks.build(bookmark_params)
-    if(bookmark.valid?)
+    if bookmark.valid?
       bookmark.save!
-      render json: {
-        message: 'ブックマーク登録に成功しました'
-      }, status: :ok
+      render status: :ok, json: { message: 'ブックマーク登録に成功しました' }
     else
-      render json: {
-        message: 'ブックマーク登録に失敗しました',
-        errors: bookmark.errors.full_messages
-      }, status: 403
+      render status: :forbidden, json: { errors: bookmark.errors.full_messages }
     end
   end
 
   def destroy
     if @bookmark.valid?
       @bookmark.destroy
-      render json: {
-        message: 'ブックマーク解除に成功しました'
-      }, status: :ok
+      render status: :ok, json: { message: 'ブックマーク解除に成功しました' }
     else
-      render json: {
-        message: 'ブックマーク解除に失敗しました',
-        errors: @bookmark.errors.full_messages
-      }, status: 403
+      render status: :forbidden, json: { errors: @bookmark.errors.full_messages }
     end
   end
 
   private
 
-  def bookmark_params
-    params.permit(:office_id)
-  end
+    def bookmark_params
+      params.permit(:office_id)
+    end
 
-  def set_bookmark
-    @bookmark = current_customer.bookmarks.find(params[:id])
-  end
+    def set_bookmark
+      @bookmark = current_customer.bookmarks.find(params[:id])
+    end
 
-  def get_office_from_bookmarks(bookmarks)
-    offices_id = []
-    bookmarks.each_with_index.map{|bookmark, index|
-      offices_id.push(bookmark.office_id)
-    }
-    offices = Office.find(offices_id)
-    offices
-  end
+    def get_office_from_bookmarks(bookmarks)
+      offices_id = []
+      bookmarks.each_with_index.map do |bookmark, _index|
+        offices_id.push(bookmark.office_id)
+      end
+      Office.find(offices_id)
+    end
 
-  def build_json(offices)
-    result = offices.each_with_index.map{|office, index|
-     superClass = Api::Customer::BookmarksController.new
-     thank = superClass.build_json_from_thank_table_attributes(office)
-     detail = superClass.build_json_from_detail_table_attributes(office)
-     staff_count = superClass.build_json_from_staff_table_count_json(office)
-     bookmark    = build_json_from_bookmark_table(office)
-     image       = superClass.build_json_image(office)
-		 count = current_customer.bookmarks.count
-     office      = office.attributes
+    def build_json(offices)
+      offices.each_with_index.map do |office, index|
+        thank = { thank: { comments: office.latest_thank_comment } }
+        detail = { detail: office.detail }
+        staff_count = { staffCount: office.staff_count }
+        bookmark    = { bookmark: Bookmark.search_office_bookmark(office.id, current_customer.id).first }
+        image       = { image: office.first_image_url }
+        count = current_customer.bookmarks.count
+        office      = office.attributes
 
-     result = if(index == 0)
-      office.merge(thank, detail, image, staff_count, bookmark, {count: count})
-              else
-                office.merge(thank, detail, image, staff_count, bookmark)
-              end
-    }
-    result
-  end
-
-  def build_json_from_bookmark_table(office)
-    bookmarks = office.bookmarks.where(user_id: current_customer.id)
-    { bookmark: bookmarks.first }
-  end
+        result = if index.zero?
+                   office.merge(thank, detail, image, staff_count, bookmark, { count: count })
+                 else
+                   office.merge(thank, detail, image, staff_count, bookmark)
+                 end
+        result
+      end
+    end
 end
