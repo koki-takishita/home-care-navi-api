eb46ab2a907cc269b9a6b719ba8847fb635a68be
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-23 12:08:38 +0900

    バリテーションメッセージのフォーマットを変更する設定をtrueにセット
    デフォルトのフォーマットでは必ず属性が先頭に表示されてしまうため

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Api
     config.active_record.default_timezone = :local
     config.api_only = true
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.active_model.i18n_customize_full_message = true
   end
 end
