c301ff7c5a1d71a537a51362c134564ae1b07b81
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-23 12:21:34 +0900

    rubocop 修正

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -5,8 +5,6 @@ class Thank < ApplicationRecord
   validates :comments, presence: true, length: { maximum: 120 }
   validates :name, length: { maximum: 30 }
   validates :name, :age, presence: true
-  validates :staff_id, uniqueness: { scope: [ :user_id, :office_id ],
-    message: "同じ%{attribute}にお礼を作成できるのは１回までです"
-  }
-  scope :thank_list_of_office, ->(office_id) { eager_load(:staff, :office).where(office_id: office_id).order("thanks.updated_at DESC") }
+  validates :staff_id, uniqueness: { scope: %i[user_id office_id] }
+  scope :thank_list_of_office, ->(office_id) { eager_load(:staff, :office).where(office_id: office_id).order('thanks.updated_at DESC') }
 end
