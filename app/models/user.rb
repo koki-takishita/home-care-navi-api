6c626c0c7bcd546dfb6fe1576e11d118f8a85406
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-23 14:07:24 +0900

    build: ユーザーモデルテスト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,16 @@ class User < ApplicationRecord
 
   enum user_type: { customer: 0, specialist: 1 }
 
-  validates :name, :phone_number, :post_code, :address, :email, presence: true
-  validates :phone_number, :email, uniqueness: true
+  with_options presence: true do
+    validates :name,  length: { maximum: 30 }
+    validates :email, uniqueness: true,
+                      length: { maximum: 255 },
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :phone_number, uniqueness: true,
+                             format: { with: /\A\d{2,4}-\d{2,4}-\d{4}\z/ }
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :address
+  end
 
   scope :phone_number_exist?, ->(phone_number) { where(phone_number: phone_number) }
   # override devise method to include additional info as opts hash
