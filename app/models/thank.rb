06d76260a4f19436ba9611b189b977a83f012f86
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-23 12:10:30 +0900

    同じスタッフにお礼を作成したときのフラッシュメッセージ変更
    変更前のメッセージではユーザーに同じスタッフお礼を作成できない意図が伝わらないため

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -5,9 +5,8 @@ class Thank < ApplicationRecord
   validates :comments, presence: true, length: { maximum: 120 }
   validates :name, length: { maximum: 30 }
   validates :name, :age, presence: true
-  validates :office_id, uniqueness: {
-    scope: [ :user_id, :staff_id ],
-      message: "に2回以上お礼は作成できません。"
-    }
+  validates :staff_id, uniqueness: { scope: [ :user_id, :office_id ],
+    message: "同じ%{attribute}にお礼を作成できるのは１回までです"
+  }
   scope :thank_list_of_office, ->(office_id) { eager_load(:staff, :office).where(office_id: office_id).order("thanks.updated_at DESC") }
 end
