c30470a06780867f0305007142b051e5bae3f35b
tmhr0 <falsity_0330@yahoo.co.jp>
2022-08-23 16:21:37 +0900

    rubocop自動修正

--- spec/requests/api/customer/contacts_spec.rb
+++ spec/requests/api/customer/contacts_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::Contacts', type: :request do
-    context '有効な情報' do
-        let(:contact) { create :contact }
-        it 'お問い合わせを登録できる' do
-          post '/api/contacts',
-            params: {
-              name: contact.name,
-              email: contact.email,
-              types: contact.types,
-              content: contact.content,
-            }
-            expect(Contact.count).to eq(2)
-            expect(response).to have_http_status(:ok)
-            expect{ create :contact }.to change { Contact.count }.by(1)
+  context '有効な情報' do
+    let(:contact) { create :contact }
 
-        end
+    it 'お問い合わせを登録できる' do
+      post '/api/contacts',
+           params: {
+             name: contact.name,
+             email: contact.email,
+             types: contact.types,
+             content: contact.content
+           }
+      expect(Contact.count).to eq(2)
+      expect(response).to have_http_status(:ok)
+      expect { create :contact }.to change { Contact.count }.by(1)
     end
-    context '空の情報' do
-        it 'お問い合わせを登録できない' do
-            post '/api/contacts',
-            params: {
-                name: '',
-                email: '',
-                types: '',
-                content: '',
-            }
-            expect(Contact.count).to eq(0)
-            expect(response).to have_http_status(:unauthorized)
-        end
+  end
+
+  context '空の情報' do
+    it 'お問い合わせを登録できない' do
+      post '/api/contacts',
+           params: {
+             name: '',
+             email: '',
+             types: '',
+             content: ''
+           }
+      expect(Contact.count).to eq(0)
+      expect(response).to have_http_status(:unauthorized)
     end
+  end
 end
-
