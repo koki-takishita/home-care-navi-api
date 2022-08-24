691fd7acec22ffe3332a488da0d1ea0e98adcdc7
tmhr0 <falsity_0330@yahoo.co.jp>
2022-08-25 07:06:44 +0900

    rubocop修正

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ApplicationRecord
-    validates :name, :email, :types, :content, presence: true
-    validates :name,  length: { maximum: 30 }
-    validates :email,  length: { maximum: 255 },
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, :email, :types, :content, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :email, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
-
