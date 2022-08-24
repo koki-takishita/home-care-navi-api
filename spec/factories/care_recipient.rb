a6762491ce0b22ddf89328d40aa4d26f7ca2c460
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-08-24 16:17:29 +0900

    build: ファクトリーボットのデータが本物っぽくなかったので本物を想定したデータに変更

--- spec/factories/care_recipient.rb
+++ spec/factories/care_recipient.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :care_recipient do
-    name { 'テスト' }
-    kana { 'かな' }    
+    name { '田中 太郎' }
+    kana { 'たなか たろう' }    
     age { 100 }
     post_code { Faker::Address.postcode } 
     address   { 'sample address' }
