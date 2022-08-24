18cda42b62f1b1e8883d5888e4862da3a3faf620
tmhr0 <falsity_0330@yahoo.co.jp>
2022-08-24 14:11:13 +0900

    APIバリデーション設定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
-  validates :name, :email, :types, :content, presence: true
+  with_options presence: true do
+    validates :name,         length: { maximum: 30 }
+    validates :email,        length: { maximum: 255 },
+                             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :types,
+    validates :content,      length: { maximum: 255 },
+  end
 end
+
