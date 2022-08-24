e814107a5b6cbea5ed6e43793f36e5eef085c9a8
tmhr0 <falsity_0330@yahoo.co.jp>
2022-08-24 12:30:05 +0900

    fix: 有効データ１件で取得

--- spec/requests/api/customer/contacts_spec.rb
+++ spec/requests/api/customer/contacts_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Contacts', type: :request do
   context '有効な情報' do
-    let(:contact) { create :contact }
-
     it 'お問い合わせを登録できる' do
       post '/api/contacts',
            params: {
-             name: contact.name,
-             email: contact.email,
-             types: contact.types,
-             content: contact.content
+             name: 'サイト利用者',
+             email: 'contact@example.com',
+             types: 'ユーザー',
+             content: 'サイト利用者よりお問い合わせをします。'
            }
-      expect(Contact.count).to eq(2)
+      expect(Contact.count).to eq(1)
       expect(response).to have_http_status(:ok)
       expect { create :contact }.to change { Contact.count }.by(1)
     end
