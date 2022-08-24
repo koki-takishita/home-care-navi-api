0d4674b44b3f287cba10176cdb8b06cc8904bd82
tmhr0 <falsity_0330@yahoo.co.jp>
2022-08-25 07:03:50 +0900

    問い合わせモデルテスト成功

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,7 @@
 class Contact < ApplicationRecord
-  with_options presence: true do
-    validates :name,         length: { maximum: 30 }
-    validates :email,        length: { maximum: 255 },
-                             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :types,
-    validates :content,      length: { maximum: 255 },
-  end
+    validates :name, :email, :types, :content, presence: true
+    validates :name,  length: { maximum: 30 }
+    validates :email,  length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
 
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :contact do
+     name { Faker::Name.name }
+     email { 'contact@example.com' }
+     types { 'ユーザー' }
+     content { 'サイト利用者よりお問い合わせをします。' }
+    end
+   end
