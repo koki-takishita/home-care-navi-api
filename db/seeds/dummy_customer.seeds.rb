32aa339fd5722ff523d1a72947cdb97b900c71d3
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-25 15:08:03 +0900

    fix: rubocop自動修正

--- db/seeds/dummy_customer.seeds.rb
+++ db/seeds/dummy_customer.seeds.rb
@@ -1,4 +1,4 @@
-require "csv"
+require 'csv'
 
 def data_init
   @email_hash      = []
@@ -9,7 +9,7 @@ end
 
 def set_data
   data_init
-  CSV.foreach("db/dummy_files/dummy.csv", headers: true) do |row|
+  CSV.foreach('db/dummy_files/dummy.csv', headers: true) do |row|
     @email_hash      << row['email']
     @fax_number_hash << row['phone_number']
     @post_code_hash  << row['post_code']
@@ -17,28 +17,29 @@ def set_data
   end
 end
 
-def set_progressbar(total: nil)
-  @progressbar = ProgressBar.create(:format => '%a |%b>>%i| %p%% %t',
-                                   :starting_at => 0,
-                                   :total => total,
-                                   :length => 80)
+def progressbar(total: nil)
+  @progressbar = ProgressBar.create(format: '%a |%b>>%i| %p%% %t',
+                                    starting_at: 0,
+                                    total: total,
+                                    length: 80)
 end
 
 def create_customer(count: 100)
   User.customer.destroy_all
-  set_progressbar(total: count)
-  count.times{|n|
+  progressbar(total: count)
+  count.times do |n|
     c = Customer.create!(
-          name:         Faker::Name.name,
-          phone_number: "#{rand(10..9999)}-#{rand(10..9999)}-#{rand(1000..9999)}",
-          post_code:    @post_code_hash[n],
-          address:      @address_hash[n],
-          email:        @email_hash[n],
-          password:              'password',
-          password_confirmation: 'password')
+      name: Faker::Name.name,
+      phone_number: "#{rand(10..9999)}-#{rand(10..9999)}-#{rand(1000..9999)}",
+      post_code: @post_code_hash[n],
+      address: @address_hash[n],
+      email: @email_hash[n],
+      password: 'password',
+      password_confirmation: 'password'
+    )
     c.confirm
     @progressbar.increment
-  }
+  end
 end
 
 set_data
