0792c435ae50038825bd469c50b1d7e79233b0dd
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-26 12:50:27 +0900

    build: 施設概要のFactorybot作成

---
+++ spec/factories/office_detail.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :office_detail do
+    detail  { '広々とした空間と安全な最新設備をご用意いたしました。' }
+    service_type { '介護付きホーム' }
+    open_date { Time.zone.today }
+    rooms { rand(0..100) }
+    requirement      { '満60歳以上の方' }
+    facility    { 'エントランス、個室、大浴場' }
+    management { Faker::Company.name }
+    link   { Faker::Internet.url }
+    images { [Rack::Test::UploadedFile.new('spec/fixtures/island.png')] * 2 }
+    comment_1 { '特徴画像1のコメント' }
+    comment_2 { '特徴画像2のコメント' }
+
+    association :office
+  end
+end
