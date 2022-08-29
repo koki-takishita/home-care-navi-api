7a682bd231aea675519114dd4ed9f154934122a2
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-29 12:32:02 +0900

    rubocopで指摘された点修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     # メールリンクのリダイレクトURLのドメインが、許可されていないドメインだったら例外をコールする
     # 許可されているドメインの指定は環境変数: MY_APP_FRONT_DOMAINで指定する
     def redirect_judgment
-      redirect_url  = @_request.query_parameters[:redirect_url]
+      redirect_url = @_request.query_parameters[:redirect_url]
       uri = URI.parse(redirect_url)
       return redirect_to(redirect_url, allow_other_host: true) if uri.host.eql?(ENV['MY_APP_FRONT_DOMAIN'])
 
