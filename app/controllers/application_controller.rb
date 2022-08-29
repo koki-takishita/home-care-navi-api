dc7715ef4b757502cacb0db59b38ecb955de5f7e
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-29 11:23:01 +0900

    redirect_toメソッドでリダイレクトを許可するドメインを設定できるように実装
    新規登録のメール認証時に指定したurlにリダイレクトするが、リダイレクトするホストがAPI側のHOSTとことなると、ActionController::Redirecting::UnsafeRedirectErrorがコールされてしまうため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  rescue_from ActionController::Redirecting::UnsafeRedirectError, with: :redirect_judgment
+
+  private
+
+    # メールリンクのリダイレクトURLのドメインが、許可されていないドメインだったら例外をコールする
+    # 許可されているドメインの指定は環境変数: MY_APP_FRONT_DOMAINで指定する
+    def redirect_judgment
+      redirect_url  = @_request.query_parameters[:redirect_url]
+      uri = URI.parse(redirect_url)
+      return redirect_to(redirect_url, allow_other_host: true) if uri.host.eql?(ENV['MY_APP_FRONT_DOMAIN'])
+
+      raise ActionController::Redirecting::UnsafeRedirectError, 'リダイレクトしようとしている、ドメインは許可されていません'
+    end
 end
